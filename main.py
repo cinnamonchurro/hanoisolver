6a1b1ffb686fef21f879f99ebd74c4d99aae4d1f
Isaac Lew <lewyzisaac@gmail.com>
2020-10-05 14:18:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def hanoisolver(t=3,r=8) : 
+    #n is the number of towers and r is the number of disks
+    towerd = {}
+    for tc in range(t) : 
+        towerd['tower'+str(tc)] = []
+        #print(tower)
+    for rc in range(r) :
+        towerd['tower0'].append(rc)
+    print(towerd)
+    if towerd['tower0'][-1]
+
+hanoisolver()
